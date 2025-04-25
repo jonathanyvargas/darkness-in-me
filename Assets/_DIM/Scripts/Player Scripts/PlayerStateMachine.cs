067a0261c83596527e6dec6c74a3a5c046ee01cf
Bellowsmay <Alimalex1525@gmail.com>
2025-04-24 22:43:03 -0700

    Fixed Jumping
    Fixed jumping by adding a Ground layer and setting all platforms and the player script to refer to said layer.

--- Assets/_DIM/Scripts/Player Scripts/PlayerStateMachine.cs
+++ Assets/_DIM/Scripts/Player Scripts/PlayerStateMachine.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStateMachine : MonoBehaviour
+public class PlayerStateMachine// : MonoBehaviour This was originally monobehavior. But we can't use "new" to make this if it is
 {
     public PlayerState currentState { get; private set; }
     
