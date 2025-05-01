cfc4844d545c8de7f3d3484770a871c6a47aae4a
Bellowsmay <Alimalex1525@gmail.com>
2025-04-30 21:31:32 -0700

    Resolved minor bugs

--- Assets/_DIM/Scripts/Player Scripts/PlayerStateMachine.cs
+++ Assets/_DIM/Scripts/Player Scripts/PlayerStateMachine.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStateMachine : MonoBehaviour
+public class PlayerStateMachine// : MonoBehaviour
 {
     public PlayerState currentState { get; private set; }
     
